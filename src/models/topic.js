aa8804994a7d9782a5f7ff2c37c16376610a401f
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2021-01-08 23:32:57 +0100

    Added question and topic model

---
+++ src/models/topic.js
@@ -0,0 +1,21 @@
+import { Schema, model } from 'mongoose';
+
+const Topic = model('Topic', new Schema (
+  {
+    _id: { 
+      type: String,
+      lowercase: true,
+      require: [true, 'Topic is required']
+    },
+    children: [
+      {
+        type: String,
+        index: true,
+        lowercase: true,
+        default: []
+      }
+    ]
+  }
+))
+
+export default Topic;
