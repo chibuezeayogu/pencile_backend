526b51037b0573e7132d550691c820685a4bc70a
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2021-01-09 01:36:36 +0100

    minor code refactor

--- src/config/config.js
+++ src/config/config.js
@@ -1,25 +1,21 @@
 import mongoose from 'mongoose';
 import dotenv  from 'dotenv';
-import { exit } from 'process';
 
 dotenv.config();
 let dbURL;
 
-if(process.env.NODE_ENV === 'developement') {
+if(process.env.NODE_ENV === 'development') {
   dbURL=`mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
 } else if(process.env.NODE_ENV === 'production') {
   dbURL=process.env.PRD_DB_URL.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 }
  
 
-mongoose.connect(dbURL, {
+const db = mongoose.connect(dbURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true
-}).then(con => {
-  console.log('DB connected successfuly');
-}).catch(err => {
-  console.log(err.reason);
-  exit();
 })
+
+export default db;
