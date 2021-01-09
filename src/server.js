e6dc0e0dd6a2e83f03ab939fa75f1d87125c48ac
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2021-01-09 05:46:14 +0100

    Added app routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express, { json, urlencoded } from 'express';
 import cors from 'cors';
-import db from './config/config'
+import db from './config/config';
+import api from './routes/index';
 
 const app = express();
 const port = parseInt(process.env.Port) || 3000;
@@ -13,6 +14,7 @@ db.then(()=> {
   );
   
   app.get("/api/v1", (req, res) => res.send({ message: "Hello, Welcome to Pencil Backend app" }));
+  app.use('/api/v1', api);
   app.listen(port, () => console.log(`App listing on port ${port}`));
 })
 .catch(err => {
