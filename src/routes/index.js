500aa9dc5f822376a2556b1ebd13961a2de58e43
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2021-01-11 00:35:21 +0100

    Write intergation test

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,9 @@ import pencilController from '../controllers/PencilController';
 const router = Router();
 
 router.get('/topics', pencilController.topics);
+router.post('/topic', pencilController.topic);
 router.get('/questions', pencilController.questions);
+router.post('/question', pencilController.question);
 router.get('/search', pencilController.search);
 
 export default router;
