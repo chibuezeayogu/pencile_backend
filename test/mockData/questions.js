5d06196b92908a987b490a9c6fc8abefd0604ba0
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2021-01-11 00:32:37 +0100

    Added mock data

---
+++ test/mockData/questions.js
@@ -0,0 +1,30 @@
+const questions = [
+  {
+    _id: 1,
+    annotations: [
+      "Wave particle duality",
+      "Heisenberg's principle"
+    ]
+  },
+  {
+    _id: 2,
+    annotations: [
+      "Electron charge",
+      "Heisenberg's principle"
+    ]
+  },
+  {
+    _id: 3,
+    annotations: [
+      "General theory"
+    ]
+  },
+  {
+    _id: 4,
+    annotations: [
+      "Electron charge"
+    ]
+  },
+]
+
+export default questions;
